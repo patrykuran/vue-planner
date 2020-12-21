b75258c5715818ee3bc7542ecbe4df570c088ada
patrykuran <trykpa@gmail.com>
2020-12-21 15:08:59 +0100

    handle delete project on icon click

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  import SingleProject from "../components/SingleProject";
+  export default {
+    name: 'Home',
+    components: {SingleProject},
+    data() {
+      return {
+        projects: []
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/projects')
+        .then(res => res.json())
+        .then(data => this.projects = data)
+        .catch(error => console.log(error.message ))
+    },
+    methods: {
+      handleDelete(id) {
+        this.projects = this.projects.filter(project => project.id !== id)
+      }
+    }
   }
-}
 </script>
